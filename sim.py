ab3abc20a3f2980b2f61f8a4f871be72f0392402
jsledge19 <32575195+jsledge19@users.noreply.github.com>
2019-01-24 21:28:47 -0600

    Update sim.py

--- sim.py
+++ sim.py
@@ -47,24 +47,3 @@ def vec_angle(vec_one, vec_two):
     cos_val = dot/mag_one/mag_two
     angle = np.degrees(np.arccos(cos_val))
     return angle
-
-
-docs = ['anna.txt', 'jake.txt', 'anna_two.txt', 'jake_two.txt']
-word_arr = create_words_array(docs)
-vectors = {}
-for doc in docs:
-    vectors[doc] = text_to_vec(doc, word_arr)
-
-angles = [
-    vec_angle(vectors[docs[0]], vectors[docs[1]]),vec_angle(vectors[docs[0]], vectors[docs[2]]),
-    vec_angle(vectors[docs[0]], vectors[docs[3]]), vec_angle(vectors[docs[1]], vectors[docs[2]]),
-    vec_angle(vectors[docs[1]], vectors[docs[3]]), vec_angle(vectors[docs[2]], vectors[docs[3]])
-]
-print("The difference between {} and {} is {}".format(docs[0], docs[1], angles[0]))
-print("The difference between {} and {} is {}".format(docs[0], docs[2], angles[1]))
-print("The difference between {} and {} is {}".format(docs[0], docs[3], angles[2]))
-print("The difference between {} and {} is {}".format(docs[1], docs[2], angles[3]))
-print("The difference between {} and {} is {}".format(docs[1], docs[3], angles[4]))
-print("The difference between {} and {} is {}".format(docs[2], docs[3], angles[5]))
-
-print("The minimum angle is {}".format(min(angles)))
